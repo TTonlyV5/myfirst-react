90b28aeb5120a7275b025628bd9f4449cd87819c
TT <514263710@qq.com>
2016-10-20 21:57:08 +0800

    项目less运行

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,36 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.less');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+//获取图片路径
+// let imageDatas = require('../data/imageDatas.json');
+// let yeomanImage = require('../images/yeoman.png');
+let imageDatas = require('json!../data/imageDatas.json');
+
+// 把所有的图片名转换为图片路径
+function genImageURL(imageDataArr) {
+  for (var i = 0, j = imageDataArr.length; i < j; i++) {
+    var singleImageData = imageDataArr[i];
+    singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+
+    imageDataArr[i] = singleImageData;
+  }
+  return imageDataArr; // body...
+}
+imageDatas = genImageURL(imageDatas);
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+        <section className="stage" ref="stage">
+            <section className="img-sec">
+                {imgFigures}
+            </section>
+            <nav className="controller-nav">
+                {controllerUnits}
+            </nav>
+        </section>
     );
   }
 }
